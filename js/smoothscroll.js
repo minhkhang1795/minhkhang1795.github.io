e43f6b206f3b621349bf121da4581df478b09425
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-01-02 10:04:01 -0500

    Add more stuff

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -4,7 +4,7 @@ $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .click(function(event) {
+    .click(function (event) {
         // On-page links
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -19,7 +19,7 @@ $('a[href*="#"]')
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 400, function() {
+                }, 400, function () {
                     // Callback after animation
                     // Must change focus!
                     var $target = $(target);
@@ -35,19 +35,19 @@ $('a[href*="#"]')
         }
     });
 
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            document.getElementById("floatingBtn").style.display = "block";
-        } else {
-            document.getElementById("floatingBtn").style.display = "none";
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("floatingBtn").style.display = "block";
+    } else {
+        document.getElementById("floatingBtn").style.display = "none";
     }
+}
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
