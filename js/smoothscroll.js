6c5d0db37f34ca14cae1d2464ee84cb64b5c3c0c
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-01-07 15:05:01 -0800

    fix carousel

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -4,6 +4,7 @@ $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
+    .not('[href="#carousel"]')
     .click(function (event) {
         // On-page links
         if (
