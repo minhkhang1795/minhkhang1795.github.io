376d13f1bf00e9a1fd076194e835d1d7afea01bf
Khang Vu <vuminhkhang1995@gmail.com>
2020-09-25 22:44:48 -0400

    Remove Roboto (#18)
    * Add gitignore (#14)
    
    * Updates
    
    * Updates
    
    * Updates

--- precache-manifest.5d98b238159194fd269b0f4ffb37a1fa.js
+++ precache-manifest.5d98b238159194fd269b0f4ffb37a1fa.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2f879b3b855f2822673edf8b80784d7",
+    "revision": "b6a5b3adf024d2a787cb3594c1808f23",
     "url": "/index.html"
   },
   {
-    "revision": "ee001e26de09940553ff",
-    "url": "/static/css/main.9b63b7cc.chunk.css"
+    "revision": "428fb58b2a250270ebdc",
+    "url": "/static/css/main.33326da9.chunk.css"
   },
   {
     "revision": "989b195d8aa4f00361d8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5990e73d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ee001e26de09940553ff",
-    "url": "/static/js/main.bee1c613.chunk.js"
+    "revision": "428fb58b2a250270ebdc",
+    "url": "/static/js/main.b817f988.chunk.js"
   },
   {
     "revision": "d6981baca288d0820697",
